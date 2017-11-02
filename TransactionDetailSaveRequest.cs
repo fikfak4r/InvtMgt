1a4831ec24ee3c506a29136c284a1b524cc0bf1e
fikfak4r <fikfak4r@gmail.com>
2017-11-02 11:42:03 +0100

    Add files via upload

---
+++ TransactionDetailSaveRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Serenity.Services;
+using System.Collections.Generic;
+using MyRow = CustomerSupport.BusinessObjects.Entities.TransactionDetailRow;
+
+namespace CustomerSupport.BusinessObjects
+{
+    public class TransactionDetailSaveRequest<MyRow> : ServiceRequest
+    {
+        public List<SaveRequest<MyRow>> Entities { get; set; }
+    }
+}
