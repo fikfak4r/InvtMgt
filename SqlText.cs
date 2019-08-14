94c1b61e90babf277e3deec237661e868bb3eaca
fikfak4r <fikfak4r@gmail.com>
2019-08-14 07:52:39 +0100

    Add files via upload

---
+++ SqlText.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace QryBldr.Lib.DataMgr
+{
+    public class SqlText
+    {
+        private string query;
+        private IDbConnection connection;
+        public SqlText(IDbConnection connection, string query)
+        {
+            this.query = query;
+            this.connection = connection;
+        }
+
+
+        public IDataReader ExecuteReader()
+        {
+            IDbCommand comm = connection.CreateCommand();
+            comm.CommandText = query;
+            return comm.ExecuteReader();
+        }
+
+        public object ExecuteScalar()
+        {
+            IDbCommand comm = connection.CreateCommand();
+            comm.CommandText = query;
+            return comm.ExecuteScalar();
+        }
+
+
+        public void ExecuteNonQuery()
+        {
+            IDbCommand comm = connection.CreateCommand();
+            comm.CommandText = query;
+            comm.ExecuteNonQuery();
+        }
+    }
+
+}
